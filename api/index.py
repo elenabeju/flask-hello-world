a9188d1ab9c5c4b257e1b44f4b5a6f7b8d5cfcd7
elena <61591191+elenabeju@users.noreply.github.com>
2024-07-04 16:20:38 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,24 @@
 from flask import Flask
+from bs4 import BeautifulSoup
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    res = requests.get('https://www.kinopoisk.cx/film/777/')
+
+
+    soup = BeautifulSoup(res.text, 'html.parser')
+
+
+    script_tag = soup.find('script', language='javascript')
+    script_content = script_tag.string
+    start_index = script_content.find("document.location.href='") + len("document.location.href='")
+    end_index = script_content.find("'", start_index)
+    redirect_url = script_content[start_index:end_index]
+
+    return redirect_url
 
 @app.route('/about')
 def about():
